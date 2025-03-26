244f0ee7eb1471d8074f98ac67b79e13c9d12c6f
Jamilly Cuco <jamillysantos2507@gmail.com>
2025-03-25 22:38:21 -0300

    project2503

--- functions.js
+++ functions.js
@@ -9,3 +9,13 @@ function baixaCurriculo() {
 function enviarEmail() {
     window.location.href = "mailto:jamillycuco@gmail.com";
 }
+
+function enviarEmail2() {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let mensagem = document.getElementById("mensagem").value;
+
+    let mailtoLink = `mailto:jamillycuco@gmail.com?subject=Contato de ${nome}&body=Nome: ${nome}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
+
+    window.location.href = mailtoLink;
+}
